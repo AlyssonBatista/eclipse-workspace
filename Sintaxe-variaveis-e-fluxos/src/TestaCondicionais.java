898d6bfdce59d15788de8c53d78172104334ac82
Alysson Batista <alyssonbatista241@gmail.com>
2020-12-26 23:36:46 -0300

    2

--- Sintaxe-variaveis-e-fluxos/src/TestaCondicionais.java
+++ Sintaxe-variaveis-e-fluxos/src/TestaCondicionais.java
@@ -2,7 +2,7 @@
 public class TestaCondicionais {
 
 	public static void main(String[] args) {
-		/*
+		 
 		System.out.println("testando condicionais");
 		int idade = 16;
 		int QuantidadeDePessoas = 3;
@@ -17,16 +17,7 @@ public class TestaCondicionais {
 			}
 		}
 		
-		*/
-		double salario = 3300.0;
-
-        if(salario < 2600.0) {
-            System.out.println("A sua aliquota é de 15%");
-            System.out.println("Você pode deduzir até R$ 350");
-        }
-        if(salario < 3750.0) {
-            System.out.println("A sua aliquota é de 22,5%");
-            System.out.println("Você pode deduzir até R$ 636");
-        }
+	 
+	 
 	}
 }
