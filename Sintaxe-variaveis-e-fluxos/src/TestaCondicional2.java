31648f20d29b31c3f329f45b9e6fcb06082ea70e
Alysson Batista <alyssonbatista241@gmail.com>
2021-04-27 19:21:57 -0300

    s

--- Sintaxe-variaveis-e-fluxos/src/TestaCondicional2.java
+++ Sintaxe-variaveis-e-fluxos/src/TestaCondicional2.java
@@ -1,4 +1,22 @@
-
 public class TestaCondicional2 {
 
-}
+	public static void main(String[] args) {
+
+	    String var = "ABC";
+
+	    try {
+
+	Integer i = new Integer(var);
+
+	System.out.println(String.format("A variavél %s vale %s", i ,i ));
+
+	    } catch (NumberFormatException e) {
+
+	System.out.println(String.format("Não foi possível atribuir a variável %s para um inteiro. Mensagem de erro: %s ", var, e.getMessage()));
+
+	    }
+
+	}
+
+    }
+
